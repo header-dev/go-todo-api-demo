593ba44d68076d0b9a3200d5e37e66971f6e2542
Kritawit.Ya <headerdev@gmail.com>
2026-01-29 22:14:17 +0700

    [feat] completed create api and database

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"todo-app/todo"
+
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&todo.Todo{})
+
+	r := gin.Default()
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	handler := todo.NewTodoHandler(db)
+	r.POST("/todos", handler.NewTask)
+
+	r.Run()
+
+}
